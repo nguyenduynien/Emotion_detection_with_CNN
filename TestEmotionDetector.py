2f75a4a411010bbfd4f49e4984b706a4f08f3eef
nguyenduynien <42843173+nguyenduynien@users.noreply.github.com>
2021-10-14 21:33:00 +0700

    update
    fix some bugs

--- TestEmotionDetector.py
+++ TestEmotionDetector.py
@@ -1,8 +1,7 @@
 import cv2
 import numpy as np
 from keras.models import model_from_json
-from numpy.core.fromnumeric import argmax
-
+import time
 
 emotion_dict = {0: "Angry", 1: "Disgusted", 2: "Fearful",
                 3: "Happy", 4: "Neutral", 5: "Sad", 6: "Surprised"}
@@ -15,12 +14,13 @@ emotion_model = model_from_json(loaded_model_json)
 
 # load weights into new model
 emotion_model.load_weights("model/emotion_model.h5")
+print("Loaded model from disk")
 
 # start the webcam feed
-cap = cv2.VideoCapture(0)
+# cap = cv2.VideoCapture(0)
 
-# input is video
-# cap = cv2.VideoCapture("G:\\CVDHD\\Emotion_detection_with_CNN-main\\test.mp4")
+# pass here your video path
+cap = cv2.VideoCapture("G:\\CVDHD\\Emotion_detection_with_CNN\\test.mp4")
 
 while True:
     # Find haar cascade to draw bounding box around face
@@ -36,7 +36,6 @@ while True:
     num_faces = face_detector.detectMultiScale(
         gray_frame, scaleFactor=1.3, minNeighbors=5)
 
-    print(num_faces)
     # take each face available on the camera and Preprocess it
     for (x, y, w, h) in num_faces:
         cv2.rectangle(frame, (x, y-50), (x+w, y+h+10), (0, 255, 0), 4)
@@ -48,11 +47,38 @@ while True:
         emotion_prediction = emotion_model.predict(cropped_img)
         maxindex = int(np.argmax(emotion_prediction))
         cv2.putText(frame, emotion_dict[maxindex], (x+5, y-20),
-                    cv2.FONT_HERSHEY_SIMPLEX, 1, (255, 0, 0), 2, cv2.LINE_AA, )
+                    cv2.FONT_HERSHEY_SIMPLEX, 1, (255, 0, 0), 2, cv2.LINE_AA)
 
-    cv2.imshow('Emotion Detection', frame)
+    cv2.imshow('Video', frame)
     if cv2.waitKey(1) & 0xFF == ord('q'):
         break
-
 cap.release()
 cv2.destroyAllWindows()
+
+
+# # emotion prediction to predict a saved image from your folder
+# frame = cv2.imread('G:\CVDHD\Emotion_detection_with_CNN\convoso12.jpg',1)
+# face_detector = cv2.CascadeClassifier('haarcascades/haarcascade_frontalface_default.xml')
+# gray_frame = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+
+# # detect faces available on camera
+# num_faces = face_detector.detectMultiScale(gray_frame, scaleFactor=1.3, minNeighbors=5)
+
+# # take each face available on the camera and Preprocess it
+# for (x, y, w, h) in num_faces:
+#     cv2.rectangle(frame, (x, y-50), (x+w, y+h+10), (0, 255, 0), 4)
+#     roi_gray_frame = gray_frame[y:y + h, x:x + w]
+#     cropped_img = np.expand_dims(np.expand_dims(cv2.resize(roi_gray_frame, (48, 48)), -1), 0)
+
+#     # predict the emotions
+#     emotion_prediction = emotion_model.predict(cropped_img)
+#     maxindex = int(np.argmax(emotion_prediction))
+#     cv2.putText(frame, emotion_dict[maxindex], (x+5, y-20), cv2.FONT_HERSHEY_SIMPLEX, 1, (255, 0, 0), 2, cv2.LINE_AA)
+
+# cv2.imshow('Picture', cv2.resize(
+#             frame, (1280, 720), interpolation=cv2.INTER_CUBIC))
+
+# if cv2.waitKey(1) & 0xFF == ord('q'):
+#     cv2.destroyAllWindows()
+
+# time.sleep(7)
